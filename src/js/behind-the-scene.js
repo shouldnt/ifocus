ef0072d3902a6e47878a7a7796780e3b8667a404
Khanh <khanh.quang.t@gmail.com>
2019-08-16 23:03:00 +0700

    update

---
+++ src/js/behind-the-scene.js
@@ -0,0 +1,38 @@
+export default(function() {
+
+	$('.site-header').addClass('_black');
+
+	$('.js-fx-target').on('mouseenter', function() {
+
+		$(this).addClass('hover');
+		$(this).find('video')[0].play();
+	});
+	$('.js-fx-target').on('mouseleave', function() {
+
+		$(this).removeClass('hover');
+		$(this).find('video')[0].pause();
+	});
+
+	$('.js-fx-target').click(function() {
+
+		var videoUrl = $(this).attr('video-url');
+
+		$(this).find('iframe').attr('src', videoUrl);
+
+
+		$('#behind-scene-modal').modal('show');
+
+
+	});
+
+	$('#behind-scene-modal').on('hide.bs.modal', function() {
+		$(this).find('iframe').attr('src', '');
+	});
+
+	$('.js-about').click(function() {
+		$('html, body').animate({
+	        scrollTop: $("#footer").offset().top
+	    }, 2000);
+	})
+	
+})
